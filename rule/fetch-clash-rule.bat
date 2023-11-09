7a62b4ee4c8665a7fe48bf43fc154cc186a4d236
Amor128 <yangshifeng19@qq.com>
2023-11-09 09:58:03 +0800

    add rule fetch

---
+++ rule/fetch-clash-rule.bat
@@ -0,0 +1,4 @@
+@echo off
+REM FILEPATH: /path/to/your/file.bat
+
+curl -o "clash_rule.yaml" "https://test.biubiufly.net/link/5JQ9ALEvZVjndNGM?clash=1"
---
+++ rule/fetch-clash-rule.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# FILEPATH: /path/to/your/file.sh
+# AUTHOR: Your Name
+# DATE: 2021-09-01
+# DESCRIPTION: A brief description of what this script does.
+
+curl -o "clash_rule.yaml" "https://test.biubiufly.net/link/5JQ9ALEvZVjndNGM?clash=1"
